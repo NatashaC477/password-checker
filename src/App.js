30a0074fbe0823729a6d2e3e605bd136ac428dd8
NatashaC477 <ncordovadiba@gmail.com>
2024-06-17 20:27:58 -0500

    Update App.js with password cheker main logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(password) {
+  i = 0;
+  if (password.length > 7) {
+    i++;
+  }
+  if (password.length <= 20) {
+    i++;
+  }
+  if (/[A-Z]/.test(password)) {
+    i++; 
+  }
+  if (/[0-9]/.test(password)) {
+    i++;
+  }
+  if (/[a-z]/.test(password)) {
+    i++; 
+  }
+
+  
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
